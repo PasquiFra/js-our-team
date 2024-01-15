24b813649c35fda57ebd5435729be89c2a6d11e8
Francesco <francesco.pasquinoni@hotmail.it>
2024-01-15 17:43:35 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,43 @@
 console.log("JS OK");
 
+// elementi del DOM
+
+
+// Array dati
+
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        position: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        position: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        position: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        position: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        position: "Developer",
+        photo: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        position: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+for (member of teamMembers){
+    console.log(member)
+}
